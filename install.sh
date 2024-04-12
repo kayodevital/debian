9ad294bf06761c7ce09abb7329073f0304d5f6cc
Kayodê Vital <kayode.vital@tutanota.com>
2024-04-12 19:45:55 -0300

    update script

--- install.sh
+++ install.sh
@@ -1,56 +1,44 @@
 #!/bin/bash
 
-# Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
-
-username=$(id -u -n 1000)
-builddir=$(pwd)
-
 # Update packages list and update system
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
 # Making .config and Moving config files and background to Pictures
-cd $builddir
 git clone --bare https://github.com/kayodevital/.dotfiles.git $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+mkdir -p .dotfiles-backup && \
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .dotfiles-backup/{}
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
+xdg-user-dirs-update
 git clone https://github.com/kayodevital/wallpapers.git
-#mkdir -p /home/$username/.config
-#mkdir -p /home/$username/.fonts
-#mkdir -p /home/$username/Pictures
-#mkdir -p /home/$username/Pictures/backgrounds
-#cp -R dotconfig/* /home/$username/.config/
-#cp bg.jpg /home/$username/Pictures/backgrounds/
-#mv user-dirs.dirs /home/$username/.config
-#chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
-apt install pcmanfm nitrogen lxpolkit x11-xserver-utils unzip wget pipewire xfce4 xfce4-goodies xfce4-power-manager network-manager network-manager-gnome pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev -y
+sudo apt install pcmanfm nitrogen lxpolkit x11-xserver-utils unzip wget pipewire xfce4-power-manager network-manager network-manager-gnome pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev alsa-utils pulseaudio xorg-dev zathura ffpmeg imagemagick mpv firefox-esr sxiv syncthing pamixer dunst acpi acpid avahi-daemon dialog mtools dosfstools gvfs-backends xinput intel-microcode simple-scan bluez cups xorg xbacklight xbindkeys xvkbd -y
+sudo systemctl enable avahi-daemon
+sudo systemctl enable acpid
+sudo systemctl enable cups
+sudo systemctl enable bluetooth
 # Installing Other less important Programs
-apt install neofetch flameshot psmisc vim lxappearance fonts-noto-color-emoji lightdm redshift-gtk xfce4-screenshooter -y
+sudo apt install neofetch psmisc vim nvim lxappearance fonts-noto-color-emoji lightdm redshift-gtk slick-greeter xfce4-screenshooter libnotify-dev libnotify-bin dunst wmctrl -y
 
 # Download Theme
-apt install arc-theme -y
+sudo apt install arc-theme papirus-icon-theme -y
 wget -q0- https://git.io/papirus-icon-theme-install | sh
 wget -q0- https://git.io/papirus-folders-theme-install | sh
 papirus-folders -C cyan --theme ePapirus
 
 # Installing fonts
-cd $builddir 
-apt install fonts-font-awesome fonts-liberation fonts-ubuntu fonts-recommended -y
-apt install cargo -y
-cargo install exa
+sudo apt install fonts-font-awesome fonts-liberation fonts-ubuntu fonts-recommended -y
+sudo apt install exa -y
 
 # Reloading Font
 fc-cache -vf
 
 # Install cursor
-apt install dmz-cursor-theme -y
+sudo apt install dmz-cursor-theme -y
 
 # Install librewolf-browser
 sudo apt update && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
@@ -76,30 +64,31 @@ sudo apt install librewolf -y
 systemctl enable lightdm
 systemctl set-default graphical.target
 
-# Enable wireplumber audio service
-#sudo -u $username systemctl --user enable wireplumber.service
+mkdir ~/.config/suckless
 
 # DWM Setup
+cd ~/.config/suckless
 git clone https://github.com/kayodevital/dwm.git
 cd dwm
-make clean install
+sudo make clean install
 cp dwm.desktop /usr/share/xsessions
-cd $builddir
 
 # st setup
+cd ~/.config/suckless
 git clone https://github.com/kayodevital/st.git
 cd st
-make clean install
-cd $builddir
+sudo make clean install
 
 # slstatus setup
-git clone https://github.com/kayodevital/sltatus.git
+cd ~/.config/suckless
+git clone https://github.com/kayodevital/slstatus.git
 cd slstatus
-make clean install
-cd $builddir
+sudo make clean install
 
 # dmenu setup
+cd ~/.config/suckless
 git clone https://git.suckless.org/dmenu
 cd dmenu
-make clean install
-cd $builddir
+sudo make clean install
+
+sudo apt autoremove
